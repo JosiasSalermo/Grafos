da73aba0eeb783cacacb8e298f077a254cd26296
Josias <josiassalermojunior>
2025-01-15 20:11:15 -0300

    commit inicial Entrega 04

---
+++ Entrega04/src/Vertice.java
@@ -0,0 +1,32 @@
+public class Vertice {
+    private String nome;
+
+    public Vertice(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    @Override
+    public String toString(){
+        return nome;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+
+        Vertice vertice = (Vertice) obj;
+        return nome.equals(vertice.nome);
+    }
+
+    @Override
+    public int hashCode(){
+        return nome.hashCode();
+    }
+
+
+}
