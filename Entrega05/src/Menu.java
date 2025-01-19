7ce5a8064e7a0b6d836e7b734b3b08880ff35c52
Josias <josiassalermojunior>
2025-01-18 21:04:26 -0300

    acresentando opções no Menu

--- Entrega05/src/Menu.java
+++ Entrega05/src/Menu.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileWriter;
